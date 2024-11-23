e67325f6bd4f7b9167d64ea428ad63d904f8e135
Ryan Dremza <ryan.dremza@gmail.com>
2024-11-23 19:41:51 +0000

    communications api - restructure, integration test project

---
+++ backend/communications/test/Falc.Communications.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Falc.Communications.Api.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
